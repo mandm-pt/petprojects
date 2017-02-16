b1106c61508c3e28553b98d6757bfed3993767c6
Michel Morais <mfr_morais@hotmail.com>
2017-02-16 22:33:34 +0000

    add colors and check if file exists

--- python/GetHash/getHash.py
+++ python/GetHash/getHash.py
@@ -1,10 +1,16 @@
 import hashlib
 import argparse
-
+import os.path
+from colorama import init, Fore
+# init colorama
+init()
 supported_hashes = ['md5', 'sha1', 'sha256', 'sha512', 'sha384']
 
 
 def calculate_hash(input, hash_types=supported_hashes, compare_hash=None):
+
+    hash_types = supported_hashes if hash_types is None else [hash_types]
+
     for hash_type in hash_types:
         if hash_type == 'md5':
             hash_obj = hashlib.md5()
@@ -19,32 +25,37 @@ def calculate_hash(input, hash_types=supported_hashes, compare_hash=None):
 
         hash_obj.update(input)
         current_hash = hash_obj.hexdigest()
-        print('{} \t {}'.format(hash_type, current_hash))
+        print((Fore.LIGHTMAGENTA_EX + '{} \t ' + Fore.WHITE + '{}').format(hash_type, current_hash))
 
         if compare_hash is not None:
             if compare_hash == current_hash:
-                print('\x1b[6;30;42m' + 'Hash match!' + '\x1b[0m')
+                print(Fore.LIGHTGREEN_EX + 'Hash match!')
             else:
-                print('\x1b[6;30;42m' + 'Hash do not match!' + '\x1b[0m')
+                print(Fore.LIGHTRED_EX + 'Hash do not match!')
+
 
 parser = argparse.ArgumentParser(description='Calculate hash of a file or string.', add_help=True)
 
-parser.add_argument('-f', '--file', help='the input file to calculate the hash')
-parser.add_argument('-t', '--text', help='the input text to calculate the hash')
-parser.add_argument('-ht', '--hash-type', help='the type of the hash to be calculated', choices=supported_hashes)
-parser.add_argument('-c', '--compare', help='the hash to compare with')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-f', '--file', help='the input file to calculate the hash', default=None)
+group.add_argument('-t', '--text', help='the input text to calculate the hash', default=None)
+parser.add_argument('-ht', '--hash-type', help='the type of the hash to be calculated', choices=supported_hashes,
+                    default=None)
+parser.add_argument('-c', '--compare', help='the hash to compare with', default=None)
 
 args = parser.parse_args()
 
+data = None
 if args.file is not None:
-    with open(args.file, "rb") as binary_file:
-        data = binary_file.read()
+    if os.path.exists(args.file):
+        with open(args.file, "rb") as binary_file:
+            data = binary_file.read()
+    else:
+        print(Fore.LIGHTRED_EX + 'File not found!' + Fore.RESET)
 elif args.text is not None:
     data = str.encode(args.text)
-else:
-    data = None
 
 if data is not None:
-    calculate_hash(data, compare_hash=args.compare)
+    calculate_hash(data, hash_types=args.hash_type, compare_hash=args.compare)
 else:
     parser.print_help()
